80b1db1a88e76ac7ad68e68ada6fe183e3d25dde
AlessandroMeschi <91841733+AlessandroMeschi@users.noreply.github.com>
2021-12-11 20:12:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,12 @@
 #include "lib.h"
+#include <iostream>
+using namespace std;
 
 float max( float massimo[], int n, float m){
-
+    
     if (n==1){
         m=massimo[0];
-
+    
     }else if(n>1){
         for(int i=1; i<n; i++){
             for(int j=1; j<=i+1; j++){
