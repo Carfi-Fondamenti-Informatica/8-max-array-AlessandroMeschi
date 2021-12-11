4bab228877bf3514f539a4815c13a3ad12c2c2c7
AlessandroMeschi <91841733+AlessandroMeschi@users.noreply.github.com>
2021-12-11 19:23:45 +0100

    Create lib.cpp

---
+++ lib.cpp
@@ -0,0 +1,20 @@
+#include "lib.h"
+
+float max( float massimo[], int n, float m){
+
+    if (n==1){
+        m=massimo[0];
+
+    }else if(n>1){
+        for(int i=1; i<n; i++){
+            for(int j=1; j<=i+1; j++){
+                if(massimo[i]>massimo[i-j]){
+                    m=massimo[i];
+                }else{
+                    m=massimo[i-j];
+                }
+            }
+        }
+    }
+    return m;
+}
