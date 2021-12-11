e76aa40f37806b44f3bbc992e6a7c1b37f13c89f
AlessandroMeschi <91841733+AlessandroMeschi@users.noreply.github.com>
2021-12-11 19:13:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,12 @@
 using namespace std;
 
 int main() {
-
     int n=0;
     float m=0;
     cin >> n;
     float massimo [n];
 
     for(int i=0; i<n; i++){
-
         float a=0;
         cin >> a;
         massimo[i]=a;
