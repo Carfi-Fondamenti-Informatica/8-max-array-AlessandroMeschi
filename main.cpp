1070604a47635185d95a6ba47b5a84628afd95fb
AlessandroMeschi <91841733+AlessandroMeschi@users.noreply.github.com>
2021-12-11 19:25:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+int main() {
+
+    int n=0;
+    float m=0;
+    cin >> n;
+    float massimo [n];
+
+    for(int i=0; i<n; i++){
+
+        float a=0;
+        cin >> a;
+        massimo[i]=a;
+
+        }
+    cout << max(massimo, n, m) <<endl;
+    return 0;
+}
